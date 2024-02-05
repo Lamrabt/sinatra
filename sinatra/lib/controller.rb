291ba0578e464a5fcb790dafdad7d9187c597884
Lamrabt <152159210+Lamrabt@users.noreply.github.com>
2024-02-05 15:57:24 +0100

    Add files via upload

---
+++ sinatra/lib/controller.rb
@@ -0,0 +1,26 @@
+require 'gossip'
+
+class ApplicationController < Sinatra::Base
+  attr_accessor :author, :content
+
+  get '/' do
+    erb :index, locals: {gossips: Gossip.all}
+  end
+
+  get '/gossips/new/' do
+    erb :new_gossip
+  end
+
+  post '/gossips/new/' do
+    Gossip.new(params["gossip_author"], params["gossip_content"]).save
+    redirect '/'
+  end
+
+  get '/gossips/:id/' do
+    erb :show, locals: {gossip: Gossip.find(params['id'])}
+  end
+  
+  get '/gossips/edit/' do
+    erb :edit
+  end
+end
